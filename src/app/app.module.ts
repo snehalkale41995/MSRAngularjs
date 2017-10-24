1702b9558bf6172abaa77c1c71ff9f096d666017
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2017-10-24 17:02:47 +0530

    Added Fundraising module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import {FundRaisingModule} from './fund-raising/fund-raising.module';
-=======
+import { AppComponent } from './app.component';
+import {ReportsModule} from './reports/reports.module';
+
 
 
 import {MembersModule } from './members/members.module';
-
->>>>>>> 412227b1cae194b32db6c7b8c02538a4b793423e
-import { AppComponent } from './app.component';
+import {FundRaisingModule} from './fund-raising/fund-raising.module';
 import { FormsModule } from '@angular/forms';
 import {RegistrationModule } from './registration/registration.module';
 import {GalleryModule} from './gallery/gallery.module';
 import {ActivityModule} from './activity/activity.module';
 
 
-
 @NgModule({
   declarations: [
-    AppComponent
-
-  ],
-  imports: [
-<<<<<<< HEAD
-    BrowserModule,
-    FundRaisingModule
-=======
+    AppComponent,
 
     BrowserModule,
-
+    FundRaisingModule,
     FormsModule,
     RegistrationModule,
     GalleryModule,
@@ -37,7 +27,6 @@ import {ActivityModule} from './activity/activity.module';
     
 
 
->>>>>>> 412227b1cae194b32db6c7b8c02538a4b793423e
   ],
   providers: [],
   bootstrap: [AppComponent]
