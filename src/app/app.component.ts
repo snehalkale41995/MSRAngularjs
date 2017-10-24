8437776e7ce4d4209eda6b91305551b8d54fcb65
Snehal Patil <snehal.patil@Eternus.local>
2017-10-24 11:46:55 +0530

    App.component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'My App';
-=======
-  title: String = 'app';
->>>>>>> 7f8e419d8bb14240db09b3f090c0ff723109d0c7
+  title= 'app';
 }
