927dec2cd38e6cc1762521977cc355fbaa1eeafa
Sagar Agale <sagar.agale@Eternus.local>
2017-10-24 16:22:08 +0530

    Report module updated..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {ReportsModule} from './reports/reports.module';
+import {MembersModule } from './members/members.module';
 import { FormsModule } from '@angular/forms';
 import {RegistrationModule } from './registration/registration.module';
 import {GalleryModule} from './gallery/gallery.module';
 import {ActivityModule} from './activity/activity.module';
-import {ReportsModule} from './reports/reports.module';
-import {MembersModule } from './members/members.module';
-
 
 
 @NgModule({
   declarations: [
     AppComponent
-
   ],
   
   imports: [
-
     BrowserModule,
-
     FormsModule,
     RegistrationModule,
     GalleryModule,
     ActivityModule,
     MembersModule,
-    ReportsModule,
-    
-
-
+    ReportsModule
   ],
   providers: [],
-
   bootstrap: [AppComponent]
 })
 export class AppModule { }
